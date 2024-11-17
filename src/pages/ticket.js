657735d0a75c3d8baad8bc69e836351f0c60c563
stronghank <stronghank@gmail.com>
2024-11-17 20:58:50 +0800

    Make the tickets only loop once

--- src/pages/ticket.js
+++ src/pages/ticket.js
@@ -1,92 +1,112 @@
-import { useState, useRef, useEffect } from 'react'
-import { Container, Row, Col, Form } from 'react-bootstrap'
-import { QRCodeSVG } from 'qrcode.react'
-import crud from '@/util/crud'
-import logger from '@/util/logger'
-import jwt from 'jsonwebtoken'
+import { useState, useRef, useEffect } from 'react';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import { QRCodeSVG } from 'qrcode.react';
+import crud from '@/util/crud';
+import logger from '@/util/logger';
+import jwt from 'jsonwebtoken';
 
 const Ticket = () => {
-    const [eventCode, setEventCode] = useState('')
-    const [tickets, setTickets] = useState(undefined)
-    const [index, setIndex] = useState(-1)
-    const [token, setToken] = useState(undefined)
+    const [eventCode, setEventCode] = useState('');
+    const [tickets, setTickets] = useState(undefined);
+    const [index, setIndex] = useState(-1);
+    const [token, setToken] = useState(undefined);
     const qrCodeRef = useRef(null);
+    const [stopTimeout, setStopTimeout] = useState(false);
+    const [downloadedIndex, setDownloadedIndex] = useState(-1);
 
-    setTimeout(() => {
-        if (tickets) {
-            let tempIndex = (index + 1) % tickets.length
-            setIndex(tempIndex)
-            jwt.sign({
-                ticket_id: tickets[tempIndex].id,
-                name: tickets[tempIndex].name,
-                email: tickets[tempIndex].email,
-                event_code: tickets[tempIndex].event_code,
-            }, process.env.NEXT_PUBLIC_JWTSECRET, { algorithm: 'HS256' }, function (err, jsonwebtoken) {
-                if (err) {
-                    logger.error(`err: ${err}`)
-                } else {
-                    setToken(jsonwebtoken)
+    useEffect(() => {
+        if (tickets && index !== -1 && !stopTimeout) {
+            const timeoutId = setTimeout(() => {
+                let tempIndex = (index + 1) % tickets.length;
+                setIndex(tempIndex);
+                jwt.sign(
+                    {
+                        ticket_id: tickets[tempIndex].id,
+                        name: tickets[tempIndex].name,
+                        email: tickets[tempIndex].email,
+                        event_code: tickets[tempIndex].event_code,
+                    },
+                    process.env.NEXT_PUBLIC_JWTSECRET,
+                    { algorithm: 'HS256' },
+                    function (err, jsonwebtoken) {
+                        if (err) {
+                            logger.error(`err: ${err}`);
+                        } else {
+                            setToken(jsonwebtoken);
+                        }
+                    }
+                );
+                logger.info(`index: ${tempIndex}`);
+
+                if (tempIndex === 0 && stopTimeout) {
+                    setStopTimeout(false); // Reset stopTimeout flag
+                } else if (tempIndex === 0) {
+                    setStopTimeout(true);
                 }
-            })
-            logger.info(`index: ${tempIndex}`)
+            }, 3000);
+
+            return () => clearTimeout(timeoutId);
         }
-    }, 3000)
+    }, [index, tickets, stopTimeout]);
 
     const handleEventCodeChange = (e) => {
-        setEventCode(e.target.value)
-    }
+        setEventCode(e.target.value);
+    };
 
     useEffect(() => {
-        Promise.all([crud({
-            action: `R`,
-            collection: `qrticket`,
-            filter: {
-                event_code: {
-                    _eq: eventCode
-                }
-            }
-        })])
-            .then(r => {
-                logger.info(`r: ${JSON.stringify(r, null, 2)}`)
-                setTickets(r[0])
-                if (r[0])
-                    setIndex(0)
-            })
-    }, [eventCode])
+        if (eventCode) {
+            crud({
+                action: `R`,
+                collection: `qrticket`,
+                filter: {
+                    event_code: {
+                        _eq: eventCode,
+                    },
+                },
+            }).then((r) => {
+                logger.info(`r: ${JSON.stringify(r, null, 2)}`);
+                setTickets(r);
+                setIndex(0);
+                setStopTimeout(false);
+                setDownloadedIndex(-1);
+            });
+        }
+    }, [eventCode]);
 
     useEffect(() => {
-        const downloadQRCode = (filename) => {
-            const canvas = document.createElement('canvas');
-            const svg = qrCodeRef.current.querySelector('svg');
-            const svgData = new XMLSerializer().serializeToString(svg);
+        if (token && index !== downloadedIndex) {
+            const currentTicket = tickets[index];
+            const filename = `${currentTicket.id}_${currentTicket.name.replace(/\s+/g, '_')}_${eventCode}`;
+            downloadQRCode(filename);
+            setDownloadedIndex(index);
+        }
+    }, [token, tickets, index, eventCode, downloadedIndex]);
 
-            const img = new Image();
-            img.onload = function () {
-                canvas.width = img.width;
-                canvas.height = img.height;
+    const downloadQRCode = (filename) => {
+        const canvas = document.createElement('canvas');
+        const svg = qrCodeRef.current.querySelector('svg');
+        const svgData = new XMLSerializer().serializeToString(svg);
 
-                const ctx = canvas.getContext('2d');
-                ctx.drawImage(img, 0, 0);
+        const img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
 
-                const dataUrl = canvas.toDataURL('image/png');
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
 
-                const link = document.createElement('a');
-                link.download = `${filename}.png`;
-                link.href = dataUrl;
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            };
+            const dataUrl = canvas.toDataURL('image/png');
 
-            img.src = 'data:image/svg+xml;base64,' + btoa(svgData);
+            const link = document.createElement('a');
+            link.download = `${filename}.png`;
+            link.href = dataUrl;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         };
 
-        if (token) {
-            const currentTicket = tickets[index];
-            const filename = `${currentTicket.id}_${currentTicket.name.replace(/\s+/g, '_')}`;
-            downloadQRCode(filename);
-        }
-    }, [token, tickets, index]);
+        img.src = 'data:image/svg+xml;base64,' + btoa(svgData);
+    };
 
     return (
         <Container className="d-flex flex-column align-items-center mt-5">
@@ -110,7 +130,7 @@ const Ticket = () => {
                 <QRCodeSVG size={256} value={token} />
             </div>
         </Container>
-    )
-}
+    );
+};
 
-export default Ticket
+export default Ticket;
